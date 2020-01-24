89e3ab9d0577d685a1e9f1437d76c7c1cfd23f46
guilhermepaitax <guilhermepaitax16@gmail.com>
2020-01-24 11:00:04 -0300

    adding cors to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app  = express();
 
-// mongoose.connect('mongodb+srv://admin:admin@cluster0-svaoa.mongodb.net/dev-radar?retryWrites=true&w=majority', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect('mongodb+srv://admin:admin@cluster0-svaoa.mongodb.net/dev-radar?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
